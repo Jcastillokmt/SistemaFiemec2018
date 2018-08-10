67d4dd3f7696464dfa54d77287f7855e12a73af8
Jcastillokmt <jcastillo@kmtadvancesystems.com>
2018-08-10 11:34:48 -0500

    Cambio de estilo al show de Catálogo
    Se realizó la mejora de la versión del catálogo en estilos.

--- SistemaFiemec/resources/views/proforma/catalogo/index.blade.php
+++ SistemaFiemec/resources/views/proforma/catalogo/index.blade.php
@@ -5,6 +5,9 @@
 	
 	@include('proforma.catalogo.search')
 </div>
+
+
+
 <div class='row'>
 	<div class='col-lg-12 col-md-12 col-sm-12 col-xs-12'>
 		<div class="table-responsive">
@@ -17,12 +20,6 @@
 					<th>Marca</th>				
                     <th>Descripcion</th>
                     <th>Precio</th>
-                 
-                    
-                   
-
-
-					
 				</thead>
 
 				@foreach ($catalogos as $pro)
