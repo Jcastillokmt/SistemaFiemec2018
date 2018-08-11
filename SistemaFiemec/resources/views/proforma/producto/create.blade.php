41c397082390e33e0d8b956aef0ef8affb3005cd
jacrproyect <jcastillo@jacrproyectosweb.com>
2018-08-10 22:25:39 -0500

    as
    sas

--- SistemaFiemec/resources/views/proforma/producto/create.blade.php
+++ SistemaFiemec/resources/views/proforma/producto/create.blade.php
@@ -13,7 +13,7 @@
     </div>
     @endif
 
-    {!!Form::open(array('url'=>'proforma/producto','method'=>'POST','autocomplete'=>'off','files'=>'true'))!!}
+    {!!Form::open(array('url'=>'proforma/producto','method'=>'POST','autocomplete'=>'off'))!!}
 
     {{Form::token()}}
 <div class="form-group">
